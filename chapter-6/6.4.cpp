d7b66833068a1e74409f670e0bbcd72712e7c179
st3phano <31676683+st3phano@users.noreply.github.com>
2021-03-21 17:31:51 -0300

    Create 6.4.cpp

---
+++ chapter-6/6.4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	char my_char {'a'};
+	int my_int {1}; 
+	double my_double {2.0};
+
+	std::cout << "Char: " << my_char << '\n';
+	std::cout << "Int: " << my_int << '\n';
+	std::cout << "Double: " << my_double << '\n';
+
+	return 0;
+}
