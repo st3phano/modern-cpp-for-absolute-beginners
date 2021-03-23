5f999168cf202f2105fc922f6c249e5ae433999f
st3phano <31676683+st3phano@users.noreply.github.com>
2021-03-23 15:40:09 -0300

    Create 15.9.cpp

---
+++ chapter-15/15.9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	auto my_char{ 'a' };
+	auto my_int{ 1 };
+	auto my_double{ 3.14 };
+
+	std::cout << "my_char: " << my_char << '\n';
+	std::cout << "my_int: " << my_int << '\n';
+	std::cout << "my_double: " << my_double << '\n';
+
+	return 0;
+}
