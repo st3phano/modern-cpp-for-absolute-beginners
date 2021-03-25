778657deb5032233149a83e02b09f14bfeed4341
st3phano <31676683+st3phano@users.noreply.github.com>
2021-03-24 21:07:58 -0300

    Create 20.1.cpp

---
+++ chapter-20/20.1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void print_message();
+
+int main()
+{
+	print_message();
+
+	return 0;
+}
+
+void print_message()
+{
+	std::cout << "Hello World from a function." << '\n';
+}
