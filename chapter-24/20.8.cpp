800bcebe092af7f4e4896c10e921db0604102517
st3phano <31676683+st3phano@users.noreply.github.com>
2021-03-27 10:14:01 -0300

    Create 20.8.cpp

---
+++ chapter-24/20.8.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+class MyClass
+{
+private:
+
+	int my_int;
+	int* my_int_ptr;
+
+public:
+
+	MyClass(int my_int_init, int my_int_ptr_init);
+	MyClass(const MyClass& copy_from);
+	void print_data();
+};
+
+// constructor
+MyClass::MyClass(int my_int_init, int my_int_ptr_init)
+	: my_int{ my_int_init },
+	  my_int_ptr{ new int{ my_int_ptr_init } }
+{
+
+}
+
+// copy constructor
+MyClass::MyClass(const MyClass& copy_from)
+	: my_int{ copy_from.my_int },
+	  my_int_ptr{ new int{ *(copy_from.my_int_ptr) } } // deep copy of my_int_ptr
+{
+
+}
+
+void MyClass::print_data()
+{
+	std::cout << "my_int: " << my_int << '\n';
+	std::cout << "my_int_ptr: 0x" << std::hex << my_int_ptr << '\n';
+	std::cout << "my_int_ptr dereferenced: " << std::dec << *my_int_ptr << '\n';
+}
+
+int main()
+{
+	MyClass o1{ 12, 34 };
+	o1.print_data();
+
+	MyClass o2{ o1 };
+	o2.print_data();
+
+	return 0;
+}
