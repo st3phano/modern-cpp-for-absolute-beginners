aaeaa37afbbf74e58ffdc99f47091758e1e3e889
st3phano <31676683+st3phano@users.noreply.github.com>
2021-03-27 09:01:22 -0300

    Create 24.3.cpp

---
+++ chapter-24/24.3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class MyClass
+{
+public:
+
+	void print_message()
+	{
+		std::cout << "Hello World" << '\n';
+	}
+};
+
+int main()
+{
+	MyClass my_object;
+
+	my_object.print_message();
+
+	return 0;
+}
