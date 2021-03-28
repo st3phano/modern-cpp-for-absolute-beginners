106922984d9d9aec3af4ee4fadbfc8d2e527cd8c
st3phano <31676683+st3phano@users.noreply.github.com>
2021-03-28 12:40:42 -0300

    Update 24.8.cpp

--- chapter-24/24.8.cpp
+++ chapter-24/24.8.cpp
@@ -10,6 +10,7 @@ private:
 public:
 
 	MyClass(int my_int_init, int my_int_ptr_init);
+	~MyClass();
 	MyClass(const MyClass& copy_from);
 	void print_data();
 };
@@ -17,15 +18,21 @@ public:
 // constructor
 MyClass::MyClass(int my_int_init, int my_int_ptr_init)
 	: my_int{ my_int_init },
-	  my_int_ptr{ new int{ my_int_ptr_init } }
+	my_int_ptr{ new int{ my_int_ptr_init } }
 {
 
+}
+
+//destructor
+MyClass::~MyClass()
+{
+	delete my_int_ptr;
 }
 
 // copy constructor
 MyClass::MyClass(const MyClass& copy_from)
 	: my_int{ copy_from.my_int },
-	  my_int_ptr{ new int{ *(copy_from.my_int_ptr) } } // deep copy of my_int_ptr
+	my_int_ptr{ new int{ *(copy_from.my_int_ptr) } } // deep copy of my_int_ptr
 {
 
 }
